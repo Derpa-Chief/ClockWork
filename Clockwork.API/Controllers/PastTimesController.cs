6e9a1f5aa2c15edae097b740f4ae46fad157423d
kelunouyang <kelun.ouyang@gmail.com>
2020-07-13 20:13:15 -0400

    Added past times controller

---
+++ Clockwork.API/Controllers/PastTimesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Clockwork.API.Models;
+
+namespace Clockwork.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class PastTimesController : Controller
+    {
+        // GET api/pasttimes
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<CurrentTimeQuery> pastTimes;
+            
+            using (var db = new ClockworkContext())
+            {
+                pastTimes = db.CurrentTimeQueries.ToList();
+            }
+            
+            return Ok(pastTimes);
+        }
+    }
+}
